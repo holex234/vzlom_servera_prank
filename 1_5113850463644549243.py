0f08c41174eace5545e114ef348b067c30c453d8
holex234 <79649460+holex234@users.noreply.github.com>
2021-02-27 23:54:49 +0200

    Update 1_5113850463644549243.py

--- 1_5113850463644549243.py
+++ 1_5113850463644549243.py
@@ -6,7 +6,7 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^.fl(?: |$)(.*)')
+@register(outgoing=True, pattern='^.hack(?: |$)(.*)')
 async def typewriter(typew):
 	message = typew.pattern_match.group(1)
 	await typew.edit("`Начинается взлом сервера...`")
